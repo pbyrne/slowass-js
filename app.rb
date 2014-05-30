11da51caf25fa0935236cbbd2baa71af10d76ace
Patrick Byrne <code@patrickbyrne.net>
2014-05-30 10:01:35 -0500

    Serve correct content type

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require "sinatra"
 
 class App < Sinatra::Application
   get "/document.write" do
+    content_type "text/javascript"
     sleep Integer(ENV.fetch("DELAY", 5))
     %Q(document.write("<div style='background: #ccc;'>slow ad</div>")\n)
   end
